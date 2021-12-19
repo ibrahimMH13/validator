7c50a6a2b91bb8c838dd1ef2e32ef3178c52aa47
Ibrahim I Musabah <i.musabah92@gmail.com>
2021-12-19 22:10:42 +0200

    set up config

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+use ibrhaim13\Validation\Validator;
+
+require_once 'vendor/autoload.php';
+
+$validator = new Validator();
+die(var_dump($validator));
---
+++ src/Validation/Validator.php
@@ -0,0 +1,6 @@
+<?php
+namespace ibrhaim13\Validation;
+class Validator
+{
+
+}
